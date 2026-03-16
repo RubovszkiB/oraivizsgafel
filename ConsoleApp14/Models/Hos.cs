7f27521654832fcec2cf84e233e03036b6a1b08c
RubovszkiB <rubovszkib@kkszki.hu>
2026-03-16 07:45:37 +0100

    first commit

---
+++ ConsoleApp14/Models/Hos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14.Models
+{
+    public class Hos
+    {
+        public string Name { get; private set; }
+        public string Tiltle { get; private set; }
+        public string Category { get; private set; }
+        public string Tag { get; private set; }
+        public double HP { get; private set; }
+        public double Attackdamage { get; private set; }
+        public double Attackdamagelevel { get; private set; }
+    }
+    public Hos(string sor) {
+            string[] str = sor.Split(';');
+            Name = str[0];
+            Tiltle = str[1];
+            Category = str[2];
+            Tag = str[3];
+            HP = double.Parse(str[4]);
+            Attackdamage = double.Parse(str[5]);
+            Attackdamagelevel = double.Parse(str[6]);
+        }
+    }
+}
